9ded66a8e9c85095ed2bdbf4da77b72e4a979795
Toni Daada <tonidaada@gmail.com>
2025-06-25 02:57:07 +0100

    modified:   src/components/SalesOverView.jsx

--- src/components/SalesOverView.jsx
+++ src/components/SalesOverView.jsx
@@ -28,11 +28,11 @@ const data = [
 
 const SalesOverView = () => {
   return (
-    <div className="flex justify-center items-center h-80 bg-gray-50 p-4 font-inter   ">
+    <div className="flex justify-center items-center h-[150] bg-gray-50 p-4 font-inter   ">
       {/* Card-like container for the chart */}
       <div className="bg-white rounded-lg shadow-xl p-6 w-full max-w-4xl  transition-transform duration-300 hover:scale-[1.1]">
         {/* Chart Header */}
-        <div className="">
+        <div className="mb-6">
           <h2 className="text-xl md:text-2xl font-bold text-gray-800">
             Sales overview
           </h2>
