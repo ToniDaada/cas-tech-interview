c47ac69dd48711c055984094e48fb0b2720ae931
Toni Daada <tonidaada@gmail.com>
2025-06-25 02:44:49 +0100

    modified:   Updated the pages layout 	modified:   Updated the SalesOverView layout

--- src/app/page.js
+++ src/app/page.js
@@ -54,13 +54,13 @@ export default function Home() {
               <Dashboard />
               {/* BarChart and SalesOverview side-by-side */}
 
-              <div className="flex flex-wrap gap-6 m-5 mb-1  ">
+              <div className="flex flex-wrap gap-6 m-5 mb-15  ">
                 <div className="w-full md:w-[calc(50%-0.75rem)] ">
                   <div className="h-full">
                     <BarChart />
                   </div>
                 </div>
-                <div className="w-full md:w-[calc(50%-0.75rem)]  h-150">
+                <div className="w-full md:w-[calc(50%-0.75rem)]  h-full">
                   <div className="h-full">
                     <SalesOverview />
                   </div>
